dd2882aeeb5bead68ed6dd2e698ffcea80effc61
Carla Cherubini <130734385+che7carla@users.noreply.github.com>
2024-10-31 17:27:15 +0100

    Update load_path_CFMs.m

--- src/lib/config/load_path_CFMs.m
+++ src/lib/config/load_path_CFMs.m
@@ -1,10 +1,9 @@
 function load_path_CFMs()
 %LOAD_PATH_CFMs Function to load all path and directory required
 %to run the script.
-addpath(genpath("..\..\..\models\"));
 addpath(genpath("..\..\..\data\"));
 addpath(genpath("..\..\..\src\lib\utility\"));
 addpath(genpath("..\..\..\src\lib\ml\"));
 addpath(genpath("..\..\..\src\lib\printer\"));
-addpath(genpath("..\..\..\reports\figures\"));
+addpath(genpath("..\..\..\src\lib\stat_analysis\"));
 end
