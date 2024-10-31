9c7db3cc6195ccd87ae25be830683db2c2535a2b
Carla Cherubini <130734385+che7carla@users.noreply.github.com>
2024-10-31 17:33:08 +0100

    Update ttest.m

--- src/t-test/ttest.m
+++ src/t-test/ttest.m
@@ -2,7 +2,12 @@
 % Used to investigate the presence of statistically significant differences 
 % between the two classes of target behaviours, Feeding and Other, in relation to 
 % the values of each of the 20 environmental features.
+clc
+clear all
 
+% Add path and directory
+addpath(genpath("..\..\lib\config\"));
+load_path_CFMs();
 
 % Run the analysis for the dataset dt_g
 perform_ttest2('dt_g.xlsx', 'T-test_results_g.xlsx', 'Significant_results_g.xlsx');
